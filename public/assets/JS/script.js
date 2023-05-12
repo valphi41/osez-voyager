2224f74fc4c82e1b2a339e3dbbd67b09c9570a10
vravanel <vravanel@gmail.com>
2023-05-12 11:48:24 +0200

    style API

--- public/assets/JS/script.js
+++ public/assets/JS/script.js
@@ -1,16 +1,66 @@
+var roulette = document.querySelector('#roulette');
+var arrow = document.querySelector('#arrow');
+var item = document.querySelector('#item');
+
+var offset = 0;
+
+var countries = [];
+
 const loadCountryAPI = () => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data => {
-        const countries = data.map(country => country.name.common);
-        const button = document.getElementById('random-country-btn');
-        button.addEventListener('click', () => {
-            const randomIndex = Math.floor(Math.random() * countries.length);
-            const randomCountry = countries[randomIndex];
-            document.getElementById('country-name').textContent = randomCountry;
+        countries = data.map(country => ({name: country.name.common, flag: country.flags.png}));
+
+        countries.forEach(function(country) {
+            var newItem = item.content.cloneNode(true);
+            newItem.querySelector('i').classList.add('fa-flag');
+            newItem.querySelector('span').textContent = country.name;
+            newItem.querySelector('.country-flag').src = country.flag;
+            roulette.appendChild(newItem);
         });
 
-    })
+        maxOffset = countries.length * 250;
+    });
 }
 
-loadCountryAPI()
+loadCountryAPI();
+
+var wheel = {
+  speed: 100,
+
+  getSpeed: function() {
+    this.speed = this.speed - Math.round(Math.random() * 1);
+    if (this.speed < 0)
+      return 0;
+    return this.speed;
+  },
+
+  resetSpeed: function() { this.speed = 100 }
+}
+
+function spin() {
+    arrow.classList.remove('shine');
+
+    var timer = setInterval(function() {
+      offset = offset + wheel.getSpeed();
+      if ((offset > maxOffset) || (offset < 0))
+        offset = 0;
+      roulette.style.transform = 'translateX(-'+offset+'px)';
+    }, 10);
+
+    setTimeout(function() {
+      clearInterval(timer);
+      wheel.resetSpeed();
+
+      var centerOffset = (roulette.offsetWidth / 2) - (250 / 2);
+      var selectedIndex = Math.ceil(offset / 250);
+      offset = selectedIndex * 250 - centerOffset;
+      roulette.style.transform = 'translateX(-'+offset+'px)';
+
+      var selectedCountry = countries[selectedIndex];
+      var rouletteSection = document.getElementById('roulette-section');
+
+      arrow.classList.add('shine');
+    }, 2500);
+  }
